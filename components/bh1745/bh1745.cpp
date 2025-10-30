e4b588bf92fd825ede8ec1325a8e2c338d88e72d
Mark Nicholls <nichollsm77@gmail.com>
2025-10-30 16:46:32 -0400

    fixes

--- components/bh1745/bh1745.cpp
+++ components/bh1745/bh1745.cpp
@@ -1,7 +1,7 @@
 #include "bh1745.h" 
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h" // Keep this one, it's needed for other macros like LOG_SENSOR
-#include "esphome/core/helpers.h" // Keep this one for logging and templating
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace bh1745 {
