796ccd0c8b152d8aeecb96cfc1fd72201f82c90d
Mark Nicholls <nichollsm77@gmail.com>
2025-10-30 16:29:29 -0400

    fixes

--- components/bh1745/bh1745.h
+++ components/bh1745/bh1745.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/core/helpers.h"
 
 namespace esphome {
 namespace bh1745 {
